5d4120a501a606d1d76bd2496dc4bb6626eb532c
SP <santapedone@router.lan>
2016-01-10 23:45:18 +0000

    fixed legend

--- layers/layers.js
+++ layers/layers.js
@@ -6,7 +6,8 @@ var jsonSource_BMEpopulation211 = new ol.source.Vector();
 jsonSource_BMEpopulation211.addFeatures(features_BMEpopulation211);var lyr_BMEpopulation211 = new ol.layer.Vector({
                 source:jsonSource_BMEpopulation211, 
                 style: style_BMEpopulation211,
-                title: "% BME population 2011"
+                attribution: "Testing 123 BME BME",
+                title: "<span> BME Population 2011<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='legend/BMEpopulation2011_18180.png'> 1.8% - 18.0%<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='legend/BMEpopulation2011_180271.png'> 18.0% - 27.1%<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='legend/BMEpopulation2011_271369.png'>  27.1% - 36.9%<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='legend/BMEpopulation2011_369480.png'>  36.9% - 48.0%<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='legend/BMEpopulation2011_480610.png'>  48.0% - 61.0%<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='legend/BMEpopulation2011_610965.png'>  61.0% - 96.5%<br></span>"
             });var format_IMD215ofLSOAsinEngland = new ol.format.GeoJSON();
 var features_IMD215ofLSOAsinEngland = format_IMD215ofLSOAsinEngland.readFeatures(geojson_IMD215ofLSOAsinEngland, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
@@ -14,7 +15,7 @@ var jsonSource_IMD215ofLSOAsinEngland = new ol.source.Vector();
 jsonSource_IMD215ofLSOAsinEngland.addFeatures(features_IMD215ofLSOAsinEngland);var lyr_IMD215ofLSOAsinEngland = new ol.layer.Vector({
                 source:jsonSource_IMD215ofLSOAsinEngland, 
                 style: style_IMD215ofLSOAsinEngland,
-                title: "IMD 2015 of LSOAs in England"
+                title: "<span> 2015 IMD by LSOAs in England<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='legend/BMEpopulation2011_18180.png'> 10% Least Deprived<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='legend/BMEpopulation2011_180271.png'> <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='legend/BMEpopulation2011_271369.png'> <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='legend/BMEpopulation2011_369480.png'> <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='legend/BMEpopulation2011_480610.png'> <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='legend/BMEpopulation2011_610965.png'> 10% Most Deprived<br></span>"
             });
 
 lyr_BMEpopulation211.setVisible(true);lyr_IMD215ofLSOAsinEngland.setVisible(true);
